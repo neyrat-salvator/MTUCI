9ca5b0e863b850d1e67c19afc251793f81016c22
neyrat-salvator <jiraftriaworld@gmail.com>
2024-12-11 00:03:27 +0300

    Переход к заданию с созданием приложения

--- KTP/src/LR/LR5_8/БСТ2257_ЮрцукКС_ЛР5.java
+++ KTP/src/LR/LR5_8/БСТ2257_ЮрцукКС_ЛР5.java
@@ -1,6 +1,5 @@
-package src.LR.LR7;
 // Юрцук Константин Сергеевич
-// Лабораторная работа №7
+// Лабораторная работа №5
 // Вариант: 26
 
 /*1.10. Вычисление функции разложением её в ряд
@@ -14,32 +13,3 @@ eps. Использовать рекуррентные соотношения п
 восходит абсолютной величины eps. Абсолютная величина суммы всех
 отброшенных членов ряда меньше eps.*/
 
-public class LR7 {
-    static int factorial(int n) {
-        int res = 1, i;
-        for (i = 2; i <= n; i++) {
-            res *= i;
-        }
-        return res;
-    }
-
-    public static void main(String[] args) {
-        double x = 0.5;
-        double eps = Math.E;
-
-        double sum = 0;
-        double term;
-        int n = 1;
-
-        do {
-            term = ((2 * n * Math.pow(x, (4 * n + 2))) / factorial(2 * n + 1));
-            sum += term;
-            n++;
-        } while (Math.abs(term) > eps);
-
-        double control = (Math.sin(Math.pow(x, 2)) - Math.pow(x, 2) * Math.cos(Math.pow(x, 2)));
-
-        System.out.printf("Сумма ряда: %.6f\n", sum);
-        System.out.printf("Контрольная формула: %.6f\n", control);
-    }
-}
