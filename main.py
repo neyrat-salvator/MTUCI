4e4ffe6384d59c61a923111042fc01d3ac7a3cf5
neyrat-salvator <jiraftriaworld@gmail.com>
2025-12-01 22:59:30 +0300

    Добавил метод для расчета 5 вариантов для вопросов по УиАИС

--- main.py
+++ main.py
@@ -5,26 +5,33 @@ from datetime import datetime
 
 
 if __name__ == '__main__':
-    # grade_book: str = '3БСТ22384'
-    # group: str = 'БСТ2257'
-    # full_name: str = 'Юрцук Константин Сергеевич'
-    # position: int = 28
     start_date: datetime = datetime.now()
     text1: str = f"""{start_date} - Программа начала свою работу.
     Программа создана исключительно в научных интересах и позволяет сравнивать свои данные в задачах с вычислениями программы.
     Программа не пересылает введенные данные на сторонние ресурсы.
     """
     print(text1)
+    
+    # Значения для дебаггинга
+    # debugging: bool = True
+    # full_name: str = 'Юрцук Константин Сергеевич'
+    # group: str = 'БСТ2257'
+    # grade_book: str = '3БСТ22384'
+    # position: int = 28
+    
+    # Значения для инициализации
+    debugging: bool = False
     full_name: str = input('Введите ФИО полностью на русском языке: ')
     group: str = input('Введите группу полностью на русском языке: ')
     grade_book: str = input('Введите номер зачетки полностью на русском языке: ')
     position: int = int(input('Введите свой актуальный номер в списке: '))
     
     student = Student(position=position, grade_book=grade_book, group=group, full_name=full_name)
-    ros: ROS = ROS(student=student)
+    student.get_5_vars()
+    # ros: ROS = ROS(student=student)
     # elpit = Elpit(student=student)
     print(student)
-    print(ros)
+    # print(ros)
     # print(elpit)
     end_date: datetime = datetime.now()
     date_diff: datetime = end_date-start_date
@@ -35,6 +42,7 @@ if __name__ == '__main__':
     Но, конечно, вы можете запустить программу снова без трудностей :)
     """
     print(text2)
-    input('Нажмите Enter для завершения программы...')
+    if not debugging:
+        input('Нажмите Enter для завершения программы...')
     
     # True
--- student.py
+++ student.py
@@ -7,14 +7,27 @@ class Student:
         self.group: str = group
         self.full_name: str = full_name
         self.position: int = position
+        self.variants_5: list = []
         # self.semesters: Semesters = Semesters(student=self)
         
     def __str__(self):
         outer_text: str = f"""Выбраны следующие параметры студента:
         1. ФИО: {self.full_name}
-        1. Группа: {self.group}
-        1. Зачетка: {self.grade_book}
-        1. Номер в списке группы: {self.position}
+        2. Группа: {self.group}
+        3. Зачетка: {self.grade_book}
+        4. Номер в списке группы: {self.position}
         """
+        if len(self.variants_5) != 0:
+            outer_text += f'5. 5 Вариантов для вопросов: {self.variants_5}'
         
         return outer_text
+    
+    def get_5_vars(self):
+        coefficient_list: list = [0, 5, 10, 15, 20]
+        outer_variants: list = []
+        for item in coefficient_list:
+            current_variant: int = self.position + item
+            outer_variants.append(current_variant)
+        self.variants_5 = outer_variants
+        
+        return outer_variants
